4cfb495df06b8327dbfef78e0e828619968f491a
guo <b22790188@gmail.com>
2025-08-05 22:58:29 +0800

    [feat] init project

---
+++ phantom_mask/src/main/java/com/example/phantom_mask/PhantomMaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.phantom_mask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhantomMaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhantomMaskApplication.class, args);
+	}
+
+}
